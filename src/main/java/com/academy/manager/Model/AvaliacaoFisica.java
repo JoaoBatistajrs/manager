66fa99d5179c150e225e7455d83b01777f6b2fc0
João Batista Santos Junior <joao.santos@programmers.com.br>
2023-02-16 09:12:18 -0300

    Adiciona entidade e conexão com banco.

--- src/main/java/com/academy/manager/Model/AvaliacaoFisica.java
+++ src/main/java/com/academy/manager/Model/AvaliacaoFisica.java
@@ -2,13 +2,43 @@ package com.academy.manager.Model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
+import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Table(name = "AvaliacoesFisicas")
 public class AvaliacaoFisica {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
   private Aluno aluno;
+
   private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
+
+  @Column(name = "peso_atual")
   private Double peso;
+
   private Double altura;
 }
